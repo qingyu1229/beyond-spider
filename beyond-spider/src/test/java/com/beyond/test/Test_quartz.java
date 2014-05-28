c0e4d9c1c14e29d028da40659e85c4a106652de0
qingyu1229 <705203544@qq.com>
2014-05-29 00:22:13 +0800

    2014-5-29 增加时间调度器 Quartz

---
+++ beyond-spider/src/test/java/com/beyond/test/Test_quartz.java
@@ -0,0 +1,16 @@
+package com.beyond.test;
+
+import com.beyond.quartz.SimpleExample;
+
+
+public class Test_quartz {
+public static void main(String[] args) {
+	  SimpleExample example = new SimpleExample();
+      try {
+		example.run();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+}
